b90d99240183362c5ff211b704f466e446696cd2
Sachio Weich <philipp-s.w@gmx.de>
2020-01-21 15:05:54 +0100

    scaffold new vue app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <section class="section">
+      <div class="container">
+        <router-view />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
