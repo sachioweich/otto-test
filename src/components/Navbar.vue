1e2da06f3ef7f9455eda06ede9b762a44af980af
Sachio Weich <philipp-s.w@gmx.de>
2020-01-24 13:08:28 +0100

    deploy website

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,17 +2,13 @@
   <b-navbar>
     <template slot="brand">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <img
-          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-          alt="Lightweight UI components for Vue.js based on Bulma"
-        />
+        <img src="../assets/catlogo.png" alt="Lightweight UI components for Vue.js based on Bulma" />
       </b-navbar-item>
     </template>
     <template slot="start">
       <b-navbar-item tag="router-link" :to="{path: '/' }">Home 🏠</b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{path: '/catfacts' }">Catfacts</b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{path: '/catquiz' }">CatQuiz</b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{path: '/about' }">About</b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{path: '/catfacts' }">Catfacts 📈</b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{path: '/catquiz' }">CatQuiz ❓</b-navbar-item>
     </template>
   </b-navbar>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -11,17 +11,9 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/catfacts',
-  name: 'catfacts',
-  component: () => import(/* webpackChunkName: "catfacts" */ '../views/Catfacts.vue')
+    name: 'catfacts',
+    component: () => import(/* webpackChunkName: "catfacts" */ '../views/Catfacts.vue')
   },
   {
     path: '/catquiz',
